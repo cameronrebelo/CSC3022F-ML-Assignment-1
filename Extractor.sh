85f1e05fb47d483e58b19058c007bfcd42919c86
RBLCAM001 <rblcam001@myuct.ac.za>
2022-05-02 22:37:21 +0200

    Added Tester Scripts

--- Extractor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for file in outputs/Classify_01/*
-do
-    python3 %extractor.py $file >> temp/percents.txt
-done
--- test_scripts/Classify_01_Tester.sh
+++ test_scripts/Classify_01_Tester.sh
@@ -3,14 +3,14 @@
 #Activation Function Tests
 
 actiation_function=" "
-for round in {4..5};
+for round in {1..3};
 do
     for mode in {1..4};
     do
-        for i in {5..20..5};
+        for i in {10..20..5};
         do
             if [ $mode -eq 1 ]; then
-                actiation_function=Tanh
+                actiation_function="Tanh"
             fi
             if [ $mode -eq 2 ]; then
                 actiation_function="Sigmoid"
@@ -21,10 +21,10 @@ do
             if [ $mode -eq 4 ]; then
                 actiation_function="LeakyReLU"
             fi
-            if [ $i -eq 5 ]; then
-                python3 src/Classify_03.py $i $mode > outputs/Classify_01/Classify_01_$actiation_function\_$round.txt
+            if [ $i -eq 10 ]; then
+                python3 src/Classify_01.py $i $mode > outputs/Classify_01/Classify_01_$actiation_function\_$round.txt
             else
-                python3 src/Classify_03.py $i $mode >> outputs/Classify_01/Classify_01_$actiation_function\_$round.txt
+                python3 src/Classify_01.py $i $mode >> outputs/Classify_01/Classify_01_$actiation_function\_$round.txt
             fi
         done
     done
